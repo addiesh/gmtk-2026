d5f763746939fb97d9b54714e92c567dd1601cfc
godot <godot@cognitohazard.thebomb.com>
2026-07-25 12:12:43 -0400

    update big build

--- rust/big-build.sh
+++ rust/big-build.sh
@@ -9,5 +9,7 @@ fi
 export GDRUST_GODOT_BIN=$(realpath ~/Godot_v4.7.1-stable_linux.x86_64)
 cargo build --target x86_64-unknown-linux-gnu $ARGS
 # cargo build --target aarch64-apple-darwin $ARGS
-cargo +nightly build --target wasm32-unknown-emscripten $ARGS
-cargo build --target x86_64-pc-windows-gnu $ARGS
+if [[ $2 = "all" ]]; then
+	cargo +nightly build --target wasm32-unknown-emscripten $ARGS
+	cargo build --target x86_64-pc-windows-gnu $ARGS
+fi
