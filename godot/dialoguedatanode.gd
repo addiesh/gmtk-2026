99ef8e4eb278c9bc209ca1ae7503feac3e239da5
godot <godot@cognitohazard.thebomb.com>
2026-07-25 16:43:12 -0400

    dial ogg

---
+++ godot/dialoguedatanode.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+#what to say
+@export var Dialogue_text: String
+#who to show
+@export var Dialogue_image: CompressedTexture2D
+
+@export var oneTime: bool
--- godot/ui/talk_box_player.gd
+++ godot/ui/talk_box_player.gd
@@ -1,16 +1,23 @@
 extends PanelContainer
 
+var dialogueDataRead
+@onready var chudTextDaughter = $HBoxContainer/RichTextLabel
+@onready var chudImageDaughter = $HBoxContainer/CenterContainer/Portrait
+
 func _ready() -> void:
 	self.hide()
 
 
-func _on_dialogue_area_entered(area: Area2D) -> void:
-	print("dialogueareaentered")
+func _on_dialogue_area_entered(area:Area2D) -> void:
+	dialogueDataRead = area.get_parent()
+	#just take the data from the dialogue data node
+	chudTextDaughter.text = dialogueDataRead.Dialogue_text
+	chudImageDaughter.texture = dialogueDataRead.Dialogue_image
+	
 	self.show()
-	#invoke ancient godot on accessing data on intruding bodies
-	#The self-destructing queue-free of the other node might be an issue
 
-
-func _on_dialogue_area_exited(area: Area2D) -> void:
-	print("dialogueareaexited")
+func _on_dialogue_area_exited(_area: Area2D) -> void:
 	self.hide()
+	#one-time dialogue box. literally just delete it.
+	if dialogueDataRead.oneTime:
+		dialogueDataRead.queue_free()
